5e0dccb8558151719c378841aa2c7af71202759d
Kristen Reid <kreiddetroit@gmail.com>
2022-06-03 06:51:18 -0400

    seeded more posts

--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -20,9 +20,57 @@ def seed_posts():
     post6 = Post(
         media_url='https://static1.cbrimages.com/wordpress/wp-content/uploads/2019/01/Samurai-Champloo.jpg', summary='A classic with an amazing soundtrack. Samurai Champloo will always be great to me.', user_id=3
         )
+    post7= Post(
+        media_url='https://www.1zoom.me/big2/31/122766-aleni.jpg', summary='Some fanart for ya', user_id=1
+    )
+    post8= Post(
+        media_url='https://i0.wp.com/images2.alphacoders.com/459/459838.jpg', summary='Show a hilarious and great show! Watched it twice lol', user_id=4
+    )
+    post9 = Post(
+        media_url='https://cdn-japantimes.com/wp-content/uploads/2019/12/p12-schley-akiraart-b-20191206.jpg', summary='This movie started my love for anime when I was a kid. Classic and still so good 🔥', user_id=3
+    )
+    post10 = Post(
+        media_url='https://cdn.vox-cdn.com/thumbor/SYiIF-623aJEsMLH5ceEnGWWQXw=/0x0:1280x1205/1200x800/filters:focal(514x213:718x417)/cdn.vox-cdn.com/uploads/chorus_image/image/64060202/tumblr_nzss2qZIyT1suxi1yo1_1280.0.jpg', summary='Jason thanks for the recommendation!', user_id=4
+    )
+    post11 = Post(
+        media_url='https://i.ytimg.com/vi/vNKy-TD1jYI/maxresdefault.jpg', summary='This show was super wild LOL can\'t believe this show was on a kids network 🤣', user_id=5
+    )
+    post12 = Post(
+        media_url='https://static1.cbrimages.com/wordpress/wp-content/uploads/2019/05/FEATURED-IMAGE-my-hero-academia.jpg', summary='Planning on binging this soon! A good friend of mine suggested I watch it 😁', user_id=6
+    )
+    post13 = Post(
+        media_url='https://cdn.wallpapersafari.com/75/22/gQDn4l.jpg', summary='SQUAD', user_id=6
+    )
+    post14 = Post(
+        media_url='https://booksandbao.com/wp-content/uploads/2021/05/jujutsu-kaisen-characters-1000x895.jpeg', summary='Heard the show and movie are fire!! Definitely plan on checking them out asap', user_id=2
+    )
+    post15 = Post(
+        media_url='https://www.anime-planet.com/images/anime/screenshots/the-god-of-high-school-14602-1.jpg', summary='Worth the watch! 💥', user_id=4
+    )
+    post16 = Post(
+        media_url='https://i.pinimg.com/originals/83/a5/75/83a575b032f4abfa1ad49e6b3a8c62c4.jpg', summary='🔥🔥🔥🔥🔥', user_id=5
+    )
+    post17 = Post(
+        media_url='https://www.slashfilm.com/img/gallery/demon-slayer-season-2-finale-a-hopeful-heartbreaking-masterpiece/l-intro-1644900696.jpg', summary='So excited for season 3!! 😭🙌🏾', user_id=1
+    )
+    post18 = Post(
+        media_url='https://www.thedigitalfix.com/wp-content/uploads/2021/10/netflix-cowboy-bebop-anime.jpg', summary='Could watch this show and listen to the soundtrack on repeat. We won\'t talk about awful live action though LOL', user_id=1
+    )
+    post19 = Post(
+        media_url='https://variety.com/wp-content/uploads/2022/03/Dragon-Ball-Z.jpg', summary='The squad I grew up with...', user_id=3
+    )
+    post20 = Post(
+        media_url='https://animeanime.global/wp-content/uploads/2021/10/438735-1.jpg', summary='Will always love this show! It deserved a better ending though...', user_id=6
+    )
+    post21 = Post(
+        media_url='https://i.pinimg.com/originals/c2/f0/0f/c2f00ff73646d344db779803f669d60b.jpg', summary='Sometimes I liked this show and sometimes it got on my nerves lol they definitely didn\'t need a spinoff lol.', user_id=2
+    )
+    post22 = Post(
+        media_url='https://i.ytimg.com/vi/Ijt145xD1TY/maxresdefault.jpg', summary='#fireforce', user_id=5
+    )
 
 
-    db.session.add_all([post1, post2, post3, post4, post5, post6])
+    db.session.add_all([post1, post2, post3, post4, post5, post6, post7, post8, post9, post10, post11, post12, post13, post14, post15, post16, post17, post18, post19, post20, post21, post22])
 
     db.session.commit()
 
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -6,14 +6,17 @@ def seed_users():
     demo = User(
        full_name='Demo Moore', username='Demo', email='demo@aa.io', password='password', image_url='https://i.pinimg.com/originals/85/5b/fa/855bfad3aee07326d0681677fc56da63.jpg')
     marnie = User(
-        full_name='Marnie Marn', username='marnie', email='marnie@aa.io', password='password')
+        full_name='Marnie Marn', username='marnie', email='marnie@aa.io', password='password', image_url='https://staticg.sportskeeda.com/editor/2022/02/7b353-16439509782499.png', description='Tryna workout until I go bald 💪🏾')
     bobbie = User(
-        full_name='Bobbie Dolo', username='bobbie', email='bobbie@aa.io', password='password')
+        full_name='Bobbie Dolo', username='bobbie', email='bobbie@aa.io', password='password', image_url='https://data.whicdn.com/images/333421372/original.jpg',description='Impatiently waiting for the 3rd season of Demon Slayer...😒')
     kris = User(
         full_name='Kris Read', username='kris', email='kris@aa.io', password='password', image_url='https://64.media.tumblr.com/94fb3cc3352fe5d471b3206de32089e2/tumblr_inline_pf1b2qQ2QU1rbr94y_500.jpg', description='Kris by day. Yoruchi by night...')
+    stevie = User(
+        full_name='Stevie Nix', username='nixie', email='nixie@aa.io', password='password', image_url='https://avatarfiles.alphacoders.com/799/79905.png')
+    eren = User(
+        full_name='Eren Yeags', username='eren', email='eren@aa.io', password='password', image_url='https://www.otaquest.com/wp-content/uploads/2021/07/eren-yeager-feature-image-1024x576.png')
 
-
-    db.session.add_all([demo, marnie, bobbie, kris])
+    db.session.add_all([demo, marnie, bobbie, kris, stevie, eren])
 
     db.session.commit()
 
