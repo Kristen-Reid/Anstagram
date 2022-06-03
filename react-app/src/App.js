67f70b429bc5bf49bea8050d56a21d0abdc054c0
Kristen Reid <kreiddetroit@gmail.com>
2022-06-03 11:59:08 -0400

    post modal

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,16 +14,17 @@ import { ReactComponent as SadFace } from "../src/svgImg/sadFace.svg";
 import { getAllPosts } from './store/post';
 import PostPage from './components/PostPage';
 import Footer from './components/Footer';
+import { getAllLikes } from './store/like';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
-  // const { id } = useParams()
-  // console.log(id)
   const user = useSelector(state => state.session.user);
 
   useEffect(() => {
-    (async() => {
+    (async () => {
+      dispatch(getAllLikes())
+      dispatch(getAllPosts())
       await dispatch(authenticate());
       setLoaded(true);
     })();
